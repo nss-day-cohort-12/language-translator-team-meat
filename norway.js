94ecfa695814213c5df1916cc6383e655273c769
Mark Zachary <markzachary@Marks-MacBook-Pro.local>
2016-01-28 10:16:58 -0600

    Added norway.js, added this to html

---
+++ norway.js
@@ -0,0 +1,28 @@
+var userInput= document.getElementById("input");
+var norwegian= document.getElementById("norwegian");
+var matrix= document.getElementById("matrix");
+var button = document.getElementById("translate");
+
+var language = (function() {
+	var words = {"Merry":"Munter",
+				 "Christmas":"Jul", 
+				 "and":"og", 
+				 "happy":"lykkelig", 
+				 "new":"ny", 
+				 "year":"år"}
+
+console.log(words.Merry, "merry");
+console.log(words.Christmas, "Christmas");
+
+	var translated = [];
+	button.addEventListener("click", function())
+
+	return 	translated.setNor = function() {
+		 words = document.innerHTML(matrix);
+		}
+	// }
+
+
+})();
+
+// translate.addEventListener("click", translateText)
